d60f6ff23bdd5ced21690111bf08d1bc4e024848
Jesper <jesperfriberg94@hotmail.com>
2020-10-01 01:31:53 +0200

    comment

--- src/Animals.java
+++ src/Animals.java
@@ -7,21 +7,23 @@ public class Animals implements IFoodAmount, IFood {
     protected int foodWeight;
     protected TypesOfFood whatFood;
 
-   //Konstruktor
-    public Animals (String name, int weight) {
+    //Konstruktor
+    public Animals(String name, int weight) {
         this.name = name;
         this.weight = weight;
     }
 
-    public String getName () {
+    public String getName() {
         return name;
     }
 
+
     @Override
-    public int getFoodAmount () {
+    public int getFoodAmount() {
         return foodWeight;
     }
 
+    // Polymorphism, metoden returnerar rätt mattyp beroende på vilket djur
     @Override
     public TypesOfFood getCorrectFood() {
         return whatFood;
